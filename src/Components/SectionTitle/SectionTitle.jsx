408cc3cacb9d6bb5b1baa20f0cceaae2040eaf61
imam0321 <imam.hossain0321@gmail.com>
2024-01-12 19:52:59 +0600

    Bistro boss client side setup

---
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,12 @@
+
+
+const SectionTitle = ({heading, subHeading}) => {
+  return (
+    <div className="md:w-3/12 text-center mx-auto my-8">
+      <p className="text-yellow-600 mb-2">--- {subHeading} ---</p>
+      <h3 className="text-3xl  uppercase border-y-4 py-2">{heading}</h3>
+    </div>
+  );
+};
+
+export default SectionTitle;
